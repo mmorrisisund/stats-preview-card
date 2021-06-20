5014d4eadd1e8830a4962bb17e045af4b1458576
Mike Morris <michael.a.morris.public@gmail.com>
2021-06-20 14:58:24 -0500

    install configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./**/*.html', './**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      desktop: { min: '375px', max: '1440px' },
+    },
+    extend: {
+      colors: {
+        'very-dark-blue': 'hsl(233, 47%, 7%)',
+        'desaturated-blue': 'hsl(244, 38%, 16%)',
+        'soft-violet': 'hsl(277, 64%, 61%)',
+        'transparent-white-50': 'hsla(0, 0%, 100%, 0.75)',
+        'transparent-white-100': 'hsla(0, 0%, 100%, 0.6)',
+      },
+      fontFamily: {
+        Inter: [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
+        Lexend: [
+          'Lexend Deca',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
+      },
+      fontSize: {
+        base: '15px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
